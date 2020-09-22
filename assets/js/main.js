3ead2a569c0f6d54e1099b7da363038d120372b1
victor.ponomarciuc <olga.2000.cristiuc@gmail.com>
2020-09-22 09:07:48 +0300

    last_commit

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function () {
-
-  $('.header-navbar__btn').on('click', function () {
-    $(this).toggleClass('header-navbar__btn--active');
-    $('.header-navbar__list').stop(true, true).fadeToggle(500);
-  });
-
-});
